943ea01760e6768642407d817c17a1a6f1a5b9de
MehrXloop <mehrunissa.pirwani@xloopdigital.com>
2023-05-06 21:44:59 +0500

    test 1 complete

--- src/main/java/VolcanoAnalyzer.java
+++ src/main/java/VolcanoAnalyzer.java
@@ -5,6 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
@@ -13,22 +15,30 @@ public class VolcanoAnalyzer {
     private List<Volcano> volcanos;
 
     public void loadVolcanoes(Optional<String> pathOpt) throws IOException, URISyntaxException {
-        try{
+        try {
             String path = pathOpt.orElse("volcano.json");
             URL url = this.getClass().getClassLoader().getResource(path);
             String jsonString = new String(Files.readAllBytes(Paths.get(url.toURI())));
             ObjectMapper objectMapper = new ObjectMapper();
             TypeFactory typeFactory = objectMapper.getTypeFactory();
-            volcanos = objectMapper.readValue(jsonString, typeFactory.constructCollectionType(List.class, Volcano.class));
-        } catch(Exception e){
-            throw(e);
+            volcanos = objectMapper.readValue(jsonString,
+                    typeFactory.constructCollectionType(List.class, Volcano.class));
+        } catch (Exception e) {
+            throw (e);
         }
     }
 
-    public Integer numbVolcanoes(){
+    public Integer numbVolcanoes() {
         return volcanos.size();
     }
 
-    //add methods here to meet the requirements in README.md
+    // add methods here to meet the requirements in README.md
+
+    // Test 1: Return the volcanoes that erupted in the 1980s.
+    public List<Volcano> eruptedInEighties() {
+        return volcanos.stream()
+                .filter(v -> v.getYear() >= 1980 && v.getYear() <= 1989)
+                .collect(Collectors.toList());
+    }
 
 }
